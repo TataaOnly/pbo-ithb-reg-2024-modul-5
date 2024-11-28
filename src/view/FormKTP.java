e237e6f98629433d0de46e7e8c298dfbd6e11419
prabhasetael <elangsatya08@gmail.com>
2024-11-28 13:54:44 +0700

    commit half gui

--- src/view/FormKTP.java
+++ src/view/FormKTP.java
@@ -37,5 +37,50 @@ public class FormKTP extends JFrame{
 
         int framex = (screenWidth/2) - (frameWidth/2);
         int framey = (screenHeigth/2)-(frameHeight/2);
+
+        frame = new JFrame("FORM KTP");
+        frame.setBounds(framex, framey, frameWidth, frameHeight);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(null);
+        inputPanel.setBounds(10,10,frameWidth, frameHeight);
+
+        JLabel NIK = new JLabel("NIK");
+        JTextField NIKField = new JTextField();
+        NIK.setBounds(50, 75, 400, 25);
+        NIKField.setBounds(150, 75, 400, 25);
+
+        inputPanel.add(NIK);
+        inputPanel.add(NIKField);
+
+        JLabel nama = new JLabel("NAMA");
+        JTextField namaField = new JTextField();
+        nama.setBounds(50, 125, 400, 25);
+        namaField.setBounds(150, 100, 400, 25);
+        inputPanel.add(nama);
+        inputPanel.add(namaField);
+
+        JLabel tpLahir = new JLabel("Tempat Lahir");
+        JTextField tempatLahirField = new JTextField();
+        tpLahir.setBounds(50, 125, 100, 25);
+        tempatLahirField.setBounds(150, 125, 400, 25);
+        inputPanel.add(tpLahir);
+        inputPanel.add(tempatLahirField);
+
+        JLabel tgLahir = new JLabel("Tanggal Lahir");
+        tgLahir.setBounds(50, 150, 400, 25);
+        inputPanel.add(tgLahir);
+
+        JLabel jenisKelamin = new JLabel("JENIS KELAMIN");
+        jenisKelamin.setBounds(50, 125, 100, 25);
+        JRadioButton jkPria = new JRadioButton("Pria");
+        jkPria.setBounds(150, 175, 50, 25);
+        JRadioButton jkWanita = new JRadioButton("Wanita");
+        jkWanita.setBounds(200, 175, 75, 25);
+
+        ButtonGroup JKbutton = new ButtonGroup();
+        JKbutton.add(jkPria);
+        JKbutton.add(jkWanita);
     }
 }
